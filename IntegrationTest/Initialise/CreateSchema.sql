5eecf5ab2de96d4fbe04c7fcf9a1ba901c8ad967
Greg Taylor <greg@bookuniverse.com>
2017-02-10 07:08:45 +1100

    Included test sproc

--- IntegrationTest/Initialise/CreateSchema.sql
+++ IntegrationTest/Initialise/CreateSchema.sql
@@ -20,3 +20,29 @@ LastName NVARCHAR(128),
 PRIMARY KEY(Id),
 FOREIGN KEY (PresidentId) REFERENCES President(Id)
 )
+
+USE [SprocMapperTest]
+GO
+/****** Object:  StoredProcedure [dbo].[GetPresidentList]    Script Date: 9/02/2017 9:13:30 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[GetPresidentList] 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT p.Id, p.FirstName, p.LastName, p.Fans, p.IsHonest, pa.PresidentId, pa.FirstName as AssistantFirstName, pa.LastName as AssistantLastName
+	FROM dbo.President p
+	LEFT JOIN dbo.PresidentAssistant pa
+	ON p.Id = pa.PresidentId
+END

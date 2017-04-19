652dc17da3e79cb687e29ed844a0bb505027e2f3
Greg Taylor <greg@cdzilla.com>
2017-04-20 08:37:01 +1000

    Missing reference

--- SprocMapperLibrary/Base/BaseSproc.cs
+++ SprocMapperLibrary/Base/BaseSproc.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using FastMember;

e2158752889a186be7dc3ca6a7d28879a048d47f
Greg Taylor <greg@bookuniverse.com>
2018-02-25 00:16:00 +1100

    .net core project support for MySql

---
+++ Crane.Shared/CacheProvider/MemoryCache/MemoryCacheProvider.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Caching;
+
+namespace Crane.CacheProvider.MemoryCache
+{
+    /// <inheritdoc />
+    public class MemoryCacheProvider : AbstractCraneCacheProvider
+    {
+        /// <inheritdoc />
+        public override bool TryGet<T>(string key, out IEnumerable<T> items)
+        {
+            lock (Padlock)
+            {
+                var res = (IEnumerable<T>)MemoryCacheSingleton.Instance[key];
+
+                if (res == null)
+                {
+                    items = default(IEnumerable<T>);
+                    return false;
+                }
+
+                lock (Padlock)
+                {
+                    items = res;
+                    return true;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Add<T>(string key, IEnumerable<T> items)
+        {
+            var cacheStrategy = GetCachingStrategy(key);
+
+            var cachePolicy = new CacheItemPolicy();
+
+            if (cacheStrategy.AbsoluteExpiration.HasValue)
+            {
+                cachePolicy.AbsoluteExpiration = cacheStrategy.InfiniteExpiration
+                    ? ObjectCache.InfiniteAbsoluteExpiration : GetDateTimeOffsetFromTimespan(cacheStrategy.AbsoluteExpiration.Value);
+            }
+
+            if (cacheStrategy.SlidingExpiration.HasValue)
+            {
+                cachePolicy.SlidingExpiration = cacheStrategy.SlidingExpiration.Value;
+            }
+
+            lock (Padlock)
+            {
+                MemoryCacheSingleton.Instance.Add(key, items, cachePolicy);
+            }
+        }
+
+        /// <inheritdoc />
+        public override void Remove(string key)
+        {
+            lock (Padlock)
+            {
+                MemoryCacheSingleton.Instance.Remove(key);
+            }
+        }
+
+        /// <inheritdoc />
+        public override void ResetCache()
+        {
+            lock (Padlock)
+            {
+                foreach (var element in MemoryCacheSingleton.Instance)
+                {
+                    MemoryCacheSingleton.Instance.Remove(element.Key);
+                }
+            }
+        }
+    }
+}
---
+++ Crane.Shared/CacheProvider/MemoryCache/MemoryCacheSingleton.cs
@@ -0,0 +1,13 @@
+﻿namespace Crane.CacheProvider.MemoryCache
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class MemoryCacheSingleton
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static System.Runtime.Caching.MemoryCache Instance { get; } = new System.Runtime.Caching.MemoryCache("CachingProvider");
+    }
+}
